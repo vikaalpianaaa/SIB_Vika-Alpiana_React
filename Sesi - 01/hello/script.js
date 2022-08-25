5f10f7475954ad0c04292767dc480117f2d96593
VIKA <111202012725@mhs.dinus.ac.id>
2022-08-26 00:05:39 +0700

    commit

--- Sesi - 01/hello/script.js
+++ Sesi - 01/hello/script.js
@@ -1,32 +1,72 @@
-alert("Hallo, Sobat Dinusian!");
-alert("DINUS INSIDE FIK 2022!!");
-
+//Deklarasi variabel dengan let
 
 let playerName = "Vika Alpiana";
 alert(playerName);
 playerName = "VA";
 alert(playerName);
 
-let message = "Universitas Dian Nuswantoro";
+//Deklarasi variabel dengan const
+
+const pi = 3.14;
+// result = 5.12; 
+
+//tipe data number
+
+alert("Tipe Data JavaScript");
+let weight = 75;
+alert(weight + 15);
+
+let originalNum = 23;
+let newNum = originalNum + 7;
+alert(newNum);
+
+//Tipe Data String
+
+alert("Helo class modern javascript!");
+
+let message = "welcome!";
 alert(message);
-message = "Teknik Informatika";
+message = "bye, bye";
 alert(message);
-message = "Udinus for a batter future";
+message = "She's a great person";
 alert(message);
 
-let score = 80
-console.log("Hasil variabel Score "+score);
+let htmlSnippet = '<h1 class="header">This is a header</h1>';
+alert("Menggabungkan String");
+let visitor = prompt("Siapakah namamu?");
+let messageVisitor = "Halo " + visitor;
+messageVisitor =
+  messageVisitor + ". Selamat datang di kelas Modern Javascript!";
+messageVisitor += " Kita senang sekali kamu mau datang berkunjung. ";
+messageVisitor +=
+  visitor + ". Semoga kamu berkenan datang kembali untuk belajar bersama.";
+alert(messageVisitor);
 
-const objectAngkaKeb = {id:1, name:'Vika Alpiana', jenkel:'P'}
-objectAngkaKeb.id = 3
-objectAngkaKeb.name = "Vika Alpiana"
-objectAngkaKeb.jenkel = "Perempuan"
-objectAngkaKeb.alamat = "Grobogan"
-console.log(objectAngkaKeb);
+//Tipe data boolean
 
-const arrayNumber = [1,2,3]
-arrayNumber.push(6)
-arrayNumber.push(7)
-console.log(arrayNumber);
+alert("Tipe Data Boolean");
+let isRaining = true;
+let isFinished = false;
 
-var _nama = "test"
+//Arrays
+
+alert("Array");
+
+alert("Deklarasi Array");
+var kode = ["React", "Php", "Ruby"];
+alert("Operasi Array");
+
+alert("Merge 2 Array");
+var fruits = ["Anggur", "Melati", "Apel", "Jeruk"];
+var buah_buahan = ["Strawberry", "Pir", "Durian"];
+alert(fruits);
+
+alert("Menghapus item pada Array");
+var indexHapus = 1;
+fruits.splice(indexHapus, 1);
+alert(fruits);
+
+alert("Mengkosongkan Array");
+var fruits = ["Anggur", "Melati", "Apel", "Jeruk"];
+fruits.splice(0, fruits.length);
+console.log(fruits);
